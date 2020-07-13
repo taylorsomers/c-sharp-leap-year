62abea8a0d6be89e1d5ad6d5fb6a8878a556360c
Taylor Somers <tsomers@yandex.com>
2020-07-13 11:14:49 -0400

    Add logic to determine how many years divisible by 4 with no remainder user has been alive

--- LeapYear.cs
+++ LeapYear.cs
@@ -1,10 +1,20 @@
 using System;
-using System.Collections.Generic; //remove if not using Dictionary in app //
 
-class Program
+class LeapYear
 {
   static void Main()
   {
-    
+    Console.WriteLine("What year were you born in?");
+    string stringBirthYear = Console.ReadLine();
+    int birthYear = int.Parse(stringBirthYear);
+
+    Console.WriteLine("You were alive during these leap years:");
+    for (int year = birthYear; year <= 2020; year ++)
+    {
+      if (year % 4 == 0)
+      {
+        Console.WriteLine(year);
+      }
+    }
   }
 }
